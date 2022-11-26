88c6993fcf494592a2a7cf6598f03c4947609389
Abdusattor10062003 <abdusattormuydinov10062003@codeschool.uz>
2022-11-27 00:17:14 +0500

    solved

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,8 @@
 # Create a function called main.
+from math import pi
+def main(a):
 # Create function arguments a.
+    a=pi
+    return a
 # Assign the value pi to the variable "a" and return.
+print(main(5))
