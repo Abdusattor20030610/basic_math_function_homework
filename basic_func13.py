60ed186e13b2e38c22c1d6f4af39bec62cb668f5
Abdusattor10062003 <abdusattormuydinov10062003@codeschool.uz>
2022-11-26 23:52:43 +0500

    solved

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,8 @@
 # Create a function called main.
+from math import sqrt
+def main(a):
 # Create function argument a.
+    a
 # Returns the square root of a number.
+    return sqrt(a)
+print(main(9))
