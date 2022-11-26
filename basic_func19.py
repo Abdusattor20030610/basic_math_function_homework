bf53e50af45d21a66beba1ce624ce6f2c597ffd8
Abdusattor10062003 <abdusattormuydinov10062003@codeschool.uz>
2022-11-27 00:22:32 +0500

    solved

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,6 @@
 # Create a function called main.
+def main(a,b):
+    return abs(a-b)
+print(main(4,11))
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
